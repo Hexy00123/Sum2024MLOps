bb037addb4ab505031280f0abfe7eaef89746602
alexandra vabnits <a.vabnits@innopolis.university>
2024-06-23 21:08:27 +0300

    wsl reference to samples.csv

--- scripts/test_data.sh
+++ scripts/test_data.sh
@@ -1,3 +1,7 @@
+# if running in wsl for the first time, running jupiter may be necessary
+# jupyter execute notebooks/data_expectations.ipynb
+# ./scripts/test_data.sh
+
 # Exit immediately if a command exits with a non-zero status
 set -e
 
@@ -9,17 +13,10 @@ DATA_SAMPLE_PATH="data/samples"
 echo "Taking a data sample..."
 python3 src/data.py project_stage=1
 
-echo "Current path: $(pwd)"
-
 # Step 2: Validate the data sample
 echo "Validating the data sample..."
 if python3 tests/data_expectations.py; then
     echo "Data validation successful."
-
-    # echo the current path
-    echo "Current path: $(pwd)"
-
-
     # Step 3: Version the data sample with DVC
     echo "Versioning the data sample with DVC..."
     dvc add $DATA_SAMPLE_PATH
