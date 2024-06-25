6cbf0346f453191cc9672da26fbb743f29766ebd
alexandra vabnits <a.vabnits@innopolis.university>
2024-06-25 19:46:35 +0300

    requirements.txt update + gx path

--- scripts/test_data.sh
+++ scripts/test_data.sh
@@ -1,15 +1,11 @@
 # if running in wsl for the first time, running jupiter may be necessary
 # Exit immediately if a command exits with a non-zero status
 set -e
-
 # Define the data sample file path
 DATA_SAMPLE_PATH="data/samples"
-
-
 # Step 1: Take a data sample
 echo "Taking a data sample..."
 python3 src/data.py project_stage=1
-
 # Step 2: Validate the data sample
 echo "Validating the data sample..."
 if python3 tests/data_expectations.py; then
