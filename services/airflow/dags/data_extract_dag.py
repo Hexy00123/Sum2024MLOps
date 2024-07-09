6f372ed10b44698138481158ab3d4f68ac7cdf60
Ruslan <r.izmajlov2018@yandex.ru>
2024-07-10 02:18:18 +0300

    airflow attempts

--- services/airflow/dags/data_extract_dag.py
+++ services/airflow/dags/data_extract_dag.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 import subprocess
 import yaml
 import os
-import git
+# import git
 
 # Define the default arguments for the DAG
 default_args = {
@@ -21,7 +21,8 @@ default_args = {
 # Function to extract a new sample of the data
 def extract_data_sample(project_stage):
     # Assuming you have a function in src/data.py for this purpose
-    subprocess.run(["python3", "src/data.py", f"index={project_stage}"], check=True)
+    subprocess.run(["python3", "src/data.py",
+                   f"index={project_stage}"], check=True)
 
 
 # Function to validate the sample using Great Expectations
@@ -31,26 +32,26 @@ def validate_data_sample():
 
 
 # Function to version the sample using DVC
-def version_data_sample(project_stage):
-    DATA_SAMPLE_PATH = "data/samples"
-    TAG = f"v{project_stage}.0"
+# def version_data_sample(project_stage):
+#     DATA_SAMPLE_PATH = "data/samples"
+#     TAG = f"v{project_stage}.0"
 
-    # DVC add
-    subprocess.run(["dvc", "add", DATA_SAMPLE_PATH], check=True)
+#     # DVC add
+#     subprocess.run(["dvc", "add", DATA_SAMPLE_PATH], check=True)
 
-    # Git add and commit using GitPython
-    repo = git.Repo('/project')
-    repo.index.add([f"{DATA_SAMPLE_PATH}.dvc"])
-    repo.index.commit(f"Add data version {TAG}")
-    origin = repo.remote(name='origin')
-    origin.push()
+#     # Git add and commit using GitPython
+#     repo = git.Repo('/project')
+#     repo.index.add([f"{DATA_SAMPLE_PATH}.dvc"])
+#     repo.index.commit(f"Add data version {TAG}")
+#     origin = repo.remote(name='origin')
+#     origin.push()
 
-    # Git tag and push tags
-    repo.create_tag(TAG, message=f"Add data version {TAG}")
-    origin.push(tags=True)
+#     # Git tag and push tags
+#     repo.create_tag(TAG, message=f"Add data version {TAG}")
+#     origin.push(tags=True)
 
 
-# Function to load the sample to the data store
+# Function to load the sample to the data store>> version_task
 def load_data_sample(project_stage):
     TAG = f"v{project_stage}.0"
     # Assuming the remote storage is already configured in your DVC remote
@@ -61,6 +62,14 @@ def load_data_sample(project_stage):
     # Update data version in YAML file
     with open('./configs/data_version.yaml', 'w') as yaml_file:
         yaml.dump({"version": TAG}, yaml_file)
+
+
+def say_hello():
+    print('hello')
+
+
+def say_bye():
+    print('bye!')
 
 
 # Define the DAG
@@ -85,11 +94,11 @@ with DAG(
         python_callable=validate_data_sample,
     )
 
-    version_task = PythonOperator(
-        task_id='version_data_sample',
-        python_callable=version_data_sample,
-        op_kwargs={'project_stage': project_stage},
-    )
+    # version_task = PythonOperator(
+    #     task_id='version_data_sample',
+    #     python_callable=version_data_sample,
+    #     op_kwargs={'project_stage': project_stage},
+    # )
 
     load_task = PythonOperator(
         task_id='load_data_sample',
@@ -97,5 +106,17 @@ with DAG(
         op_kwargs={'project_stage': project_stage},
     )
 
+    simple_task_1 = PythonOperator(
+        task_id='say_hello',
+        python_callable=say_hello,
+    )
+
+    simple_task_2 = PythonOperator(
+        task_id='say_bye',
+        python_callable=say_bye,
+    )
+
     # Set task dependencies
-    extract_task >> validate_task >> version_task >> load_task
+    # extract_task >> validate_task >> version_task >> load_task
+    # extract_task >> validate_task >> load_task
+    simple_task_1 >> simple_task_2
