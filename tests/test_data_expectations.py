57864fa24df03b97a30e9dc6c29a3a7258885dc3
Alexandra Vabnits <sashaki02@gmail.com>
2024-07-21 21:04:12 +0300

    add conftest

--- tests/test_data_expectations.py
+++ tests/test_data_expectations.py
@@ -1,33 +1,4 @@
-import pandas as pd
-import pytest
-from hydra import compose, initialize
-from omegaconf import DictConfig
-from typing import Tuple
-
-
-from src.data import read_datastore, preprocess_data
 from src.data_expectations import validate_features, validate_initial_data
-
-
-@pytest.fixture
-def cfg() -> DictConfig:
-    """
-    Load the test_config.yaml configuration file
-    """
-    with initialize(config_path="../configs", version_base=None):
-        cfg = compose(config_name="test_config")
-    return cfg
-
-
-@pytest.fixture
-def raw_sample(cfg) -> pd.DataFrame:
-    df = read_datastore(cfg)
-    return df
-
-@pytest.fixture
-def preprocessed_sample(raw_sample) -> Tuple[pd.DataFrame, pd.Series]:
-    X, y = preprocess_data(raw_sample)
-    return X, y
 
 
 def test_validate_initial_data():
