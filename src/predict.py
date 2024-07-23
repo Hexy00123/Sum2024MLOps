fb74756f1cd4dc2d838c5d85e90001a6f289eff6
Alexandra Vabnits <sashaki02@gmail.com>
2024-07-24 00:07:05 +0300

    predict entrypoint

---
+++ src/predict.py
@@ -0,0 +1,34 @@
+import json
+import requests
+import hydra
+from data import read_features
+
+@hydra.main(config_path="../configs", config_name="main", version_base=None) # type: ignore
+def predict(cfg = None):
+
+    X, y = read_features(name = "features_target", 
+                        version = cfg.example_version, 
+                        )
+
+    example = X.iloc[0,:]
+    example_target = y[0]
+
+    example = json.dumps( 
+    { "inputs": example.to_dict() }
+    )
+
+    payload = example
+
+    response = requests.post(
+        url=f"http://localhost:{cfg.port}/invocations",
+        data=payload,
+        headers={"Content-Type": "application/json"},
+    )
+
+    print(response.json())
+    print("encoded target labels: ", example_target)
+    # print("target labels: ", list(cfg.data.labels)[example_target])
+
+
+if __name__=="__main__":
+    predict()
